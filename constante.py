2e80fd32c6fd1d8f16b0a9ece72533b28832b886
David LANG <david.lang@teledetection.fr>
2017-10-17 15:52:37 +0200

    Ajout des constantes de couleurs

--- constante.py
+++ constante.py
@@ -1,6 +1,12 @@
 import rectangle
 import point
 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 obstacle = [
     rectangle.Rectangle(point.Point(20, 20), 30, 50),
