88ce1b270a9828fe04f20b227fd40bf4c6375637
Lupiac <antoine.lupiac0@gmail.com>
2017-10-20 14:51:58 +0200

    "Parsing arguments"

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import getopt
+
 import constante
 import pygame
 import time
@@ -5,6 +7,7 @@ from drawer import Drawer
 from terrain import Terrain
 from user import User
 from point import Point
+import sys
 
 
 def launch_simulation():
@@ -18,8 +21,42 @@ def launch_simulation():
     pygame.quit()
 
 
-def parse_arg():
-    None
+def parse_arg(argv):
+    p = 2 ** 9
+    t = 0
+    m = False
+    i = False
+
+    try:
+        opts, args = getopt.getopt(argv, "ip:t:m", [])
+    except getopt.GetoptError:
+        # usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            p = -1
+            # usage()
+            sys.exit()
+        elif opt == "-p":
+            p = 2 ** int(arg)
+
+        elif opt == "-t":
+            t = int(arg)
+        elif opt == "-m":
+            m = True
+        elif opt == "-i":
+            i = True
+    return p, t, m, i
+
+def main(argv):
+    arg = parse_arg(argv)
+    P = arg[0]
+    T = arg[1]
+    M = arg[2]
+    INTERFACE = arg[3]
+    print P, T, M, INTERFACE
+    # launch_simulation()
 
 if __name__ == '__main__':
-    launch_simulation()
+    main(sys.argv[1:])
+
